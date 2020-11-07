83211bce1ade53e4b9ff365d0c0a79bd2f23c030
Graham Mix <gmix@eagles.ewu.edu>
2020-11-06 21:03:26 -0800

    Added more to Main Menu

---
+++ Senior Project/Assets/StarGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarGenerator : MonoBehaviour
+{
+    public Sprite starSprite;
+    GameObject[] spriteArray;
+    int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteArray = new GameObject[10];
+        index = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        index = ++index % 10;
+    }
+}

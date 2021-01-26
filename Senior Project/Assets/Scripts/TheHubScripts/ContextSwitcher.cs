9a7f80319181d039c536b7aa26b15ac1b2924d1a
Graham Mix <51464340+Gingervitis5@users.noreply.github.com>
2021-01-26 11:13:45 -0800

    Added animations to The Hub
    Added smooth transitions between menus in The Hub.

---
+++ Senior Project/Assets/Scripts/TheHubScripts/ContextSwitcher.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ContextSwitcher : MonoBehaviour
+{
+    public GameObject FadeScreen, ControlPanel;
+    public Canvas TheHubCanvas, BlackMarketCanvas, MissionBriefingCanvas;
+
+    public void ButtonPressed()
+    {
+        FadeScreen.GetComponent<Animation>().Play("FadeAnim");
+        StartCoroutine(PlayAnimation());
+    }
+
+    IEnumerator PlayAnimation()
+    {
+        yield return new WaitForSeconds(2);
+        switch (this.gameObject.name)
+        {
+            case "Black Market":
+                ControlPanel.GetComponent<ControlPanel>().EnableBlackMarket();
+                BlackMarketCanvas.GetComponentInChildren<Animation>().Play("Black Market_Startup");
+                break;
+            case "Mission Briefing":
+                ControlPanel.GetComponent<ControlPanel>().EnableMissionBriefing();
+                Animation[] animations = MissionBriefingCanvas.GetComponentsInChildren<Animation>();
+                foreach (Animation anim in animations)
+                {
+                    anim.Play();
+                }
+                break;
+            case "Armaments":
+                Debug.Log("Armaments Pressed");
+                break;
+        }
+    }
+}

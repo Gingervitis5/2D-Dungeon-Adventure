45e58c4b2a1ab5afb5e308ec885b4779dd359d31
CalumMackenzie-EWU <57304730+CalumMackenzie-EWU@users.noreply.github.com>
2021-02-22 14:43:05 -0800

    Movement is working
    Can move along, minimap also moves, but is hidden behind background.

---
+++ Senior Project/Assets/Scripts/FirstLevelScripts/MinimapBuilder.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapBuilder : MonoBehaviour
+{
+    public Room[,] allRooms;
+    public MinimapController miniCont;
+    public GameObject[,] minimap;
+    public Sprite roomSprite;
+    public GameObject minimapParent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BuildMap()
+    {
+        minimap = new GameObject[allRooms.GetLength(0), allRooms.GetLength(1)];
+        for (int r = 0; r < allRooms.GetLength(0); r++)
+        {
+            for (int c = 0; c < allRooms.GetLength(1); c++)
+            {
+                if(allRooms[r,c].isBlocked() == false)
+                {
+                    GameObject g = new GameObject();
+                    //689, 387
+                    g.transform.position = new Vector3(r * 10, c * -10);
+                    var spriteRen = g.AddComponent<SpriteRenderer>();
+                    spriteRen.sprite = roomSprite;
+                    spriteRen.sortingOrder = -10;
+                    g.transform.SetParent(minimapParent.transform);
+                    minimap[r, c] = g;
+                }
+                
+                //s.color = new Color(r, c, r + c);
+            }
+        }
+
+        miniCont.minimap = minimap;
+       
+    }
+  
+}
---
+++ Senior Project/Assets/Scripts/FirstLevelScripts/RoomBuilder.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomBuilder : MonoBehaviour
+{
+    public Room[,] allRooms;
+    public Sprite[] listOfBackgrounds;
+    public MinimapBuilder minimapBuilder;
+    
+
+    
+    public RoomBuilder()
+    {
+        allRooms = new Room[3, 2];
+        //InitializeRooms();
+        //awake();
+    }
+    /*
+    private void Start()
+    {
+        allRooms = new Room[3, 3];
+    }
+    */
+
+    private void Awake()
+    {
+        
+        allRooms[0, 0] = new Room(listOfBackgrounds[0]);
+        allRooms[1, 0] = new Room(listOfBackgrounds[1]);
+        allRooms[2, 0] = new Room(listOfBackgrounds[2]);
+
+        allRooms[0, 1] = new Room(listOfBackgrounds[3]);
+        allRooms[1, 1] = new Room();
+        allRooms[2, 1] = new Room(listOfBackgrounds[4]);
+
+        BuildMinimap();
+
+    }
+
+    private void BuildMinimap()
+    {
+        minimapBuilder.allRooms = allRooms;
+        minimapBuilder.BuildMap();
+        
+    }
+    /*
+public bool CheckBoundaries(int x, int y)
+{
+   if(x < 0 || y < 0)
+   {
+       return false;
+   }
+   if(x > allRooms.GetLength(0))
+   {
+       return false;
+   }
+   if(y > allRooms.GetLength(1))
+   {
+       return false;
+   }
+   else
+   {
+       return true;
+   }
+}
+*/
+}

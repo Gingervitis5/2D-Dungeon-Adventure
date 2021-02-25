b8779a69dabaeb2b2046bfbec593b87741ab2881
Mix <gmix@eagles.ewu.edu>
2021-02-25 10:35:28 -0800

    Fix mac issues

--- Senior Project/Assets/Scripts/TheHubScripts/MissionBriefing/MarkerSpinner.cs
+++ Senior Project/Assets/Scripts/TheHubScripts/MissionBriefing/MarkerSpinner.cs
@@ -7,7 +7,7 @@ public class MarkerSpinner : MonoBehaviour
 
     void Update()
     {
-        this.gameObject.transform.eulerAngles += new Vector3(0, 0, (0.5f % 360));
+        this.gameObject.transform.eulerAngles += new Vector3(0, 0, (0.15f % 360));
         if (this.gameObject.transform.rotation.z > 360)
         {
             this.gameObject.transform.eulerAngles = new Vector3(0, 0, 0);

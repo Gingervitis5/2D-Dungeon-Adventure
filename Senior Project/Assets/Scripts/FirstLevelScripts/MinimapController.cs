b1a01d868cd7bae5783413b467c245260c2eddac
Mix <gmix@eagles.ewu.edu>
2021-03-01 14:44:39 -0800

    Added Calum's stuff to my branch

---
+++ Senior Project/Assets/Scripts/FirstLevelScripts/MinimapController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class MinimapController : MonoBehaviour
+{
+    //public Room [,] allRooms;
+    public GameObject[,] minimap;
+    public Sprite unfocusedRoom;
+    public Sprite focusedRoom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void updateMinimap(Point from, Point to)
+    {
+        minimap[from.X, from.Y].GetComponent<SpriteRenderer>().sprite = unfocusedRoom;
+        minimap[to.X, to.Y].GetComponent<SpriteRenderer>().sprite = focusedRoom;
+    }
+    private void Awake()
+    {
+        minimap[0, 0].GetComponent<SpriteRenderer>().sprite = focusedRoom;
+    }
+}
---
+++ Senior Project/Assets/Scripts/FirstLevelScripts/Room.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class Room : MonoBehaviour
+public class Room
+{
+    private bool blocked = true;
+    private Sprite roomImage;
+
+    public Room(Sprite backgroundPar)
+    {
+        roomImage = backgroundPar;
+        blocked = false;
+    }
+    public Room()
+    {
+        //empty Room that will remain blocked.
+    }
+    public void setBackground(Sprite background)
+    {
+        this.roomImage = background;
+        this.blocked = false;
+    }
+    public Sprite getBackground()
+    {
+        return this.roomImage;
+    }
+
+    public bool isBlocked()
+    {
+        return this.blocked;
+    }
+}
